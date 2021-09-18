03c4a835846b87ec5a2048b15ff05f04f25b6ddc
ricardo-applepie <fkondem@gmail.com>
2021-09-19 01:46:50 +0200

    created customers table in db and front end

--- function.php
+++ function.php
@@ -1,9 +1,33 @@
+
 <?php
-$con = new mysqli('localhost','root','','test');
-if($con){
-    echo 'succesful';
-}else{
-    echo die(mysqli_error());
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+ if ( !empty($_POST) ) {
+    $fullname= $_POST['name'];
+    $age= $_POST['age'];
+    $phone= $_POST['phone'];
+    $location=$_POST['location'];
+    $sql = "INSERT INTO `person` (name, age, location,phone) VALUES ('$fullname','$age','$location','$phone')";
+
+    if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+ }
 
 ?>
+
+
+
--- index.php
+++ index.php
@@ -15,56 +15,136 @@
   </head>
   <body>
 
-    <div class='container'>
-        <form>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+ <?php 
 
-        <div>
-            <table class="table table-striped table-dark">
-                <thead>
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
+ 
+ ?>
+    <div class='container'>
+
+         <div>
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+                </li>
+                
+            </ul>
+
+         </div>   
+         <div>
+             <h1 class="orders-title">Customers</h1>
+         </div>
+         <div>
+            <div class='left-menu'>
+              <ul class="list-group">
+                    <li class="list-group-item active">Cras justo odio</li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Morbi leo risus</li>
+                    <li class="list-group-item">Porta ac consectetur ac</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                    <li class="list-group-item">Morbi leo risus</li>
+                    <li class="list-group-item">Porta ac consectetur ac</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                    <li class="list-group-item">Morbi leo risus</li>
+ 
+               </ul>   
+                <div class='add-order__btn'>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                      Add Customer
+                    </button> 
+                </div>   
+            </div>
+
+             <div class='dashboard'>
+     
+            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Add new user</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="function.php" method="post">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">fullname</label>
+                            <input name="name" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="fullname">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">age</label>
+                            <input name="age" type="number" class="form-control" id="exampleInputPassword1" placeholder="age">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">location</label>
+                            <input name="location" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="location">
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">phone</label>
+                            <input name="phone"  type="text" class="form-control" id="exampleInputPassword1" placeholder="phone">
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+
+                </div>
+            </div>
+            </div>
+            <div>
+                <table class="table table-striped table-dark">
+                    <thead>
+                        <tr>
+                        <th scope="col">age</th>
+                        <th scope="col">Full Names</th>
+                        <th scope="col">Number</th>
+                        <th scope="col">Location</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                     <?php 
+                       include 'function.php';
+                       
+                    $sql = "SELECT * from `person`";
+                    $results = $conn -> query( $sql);
+   
+                      while($row = $results  -> fetch_assoc()){
+                        $age= $row["age"];
+                        $fullname= $row['name'];
+                        $phone= $row['phone'];
+                        $location=$row['location'];
+                          echo '
+                             <tr>
+                                <th scope="row">'.$age.'</th>
+                                <td>'.$fullname.'</td>
+                                <td>'.$phone.'</td>
+                                <td>'.$location.'</td>
+                            </tr>'
+                          ;
+                          
+                      }
+                   
+                     
+                     ?>
+                    </tbody>
+                </table>
+            </div> 
+        
         </div>
+           
+         </div>
+        
 </div>
 
     <!-- Optional JavaScript -->
